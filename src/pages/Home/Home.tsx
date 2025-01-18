1aa3b07dfa6bb0cc33d05731e03605ae4cb9fb08
edard3v <edard3v@gmail.com>
2025-01-18 10:20:12 -0500

    home funcional

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,20 @@
 import css from "./Home.module.css";
 import Layout from "@layouts/Layout/Layout";
 import { HOME } from "./config";
-import Btn from "@components/buttons/Btn/Btn";
-import Email from "@components/inputs/Email/Email";
-import Password from "@components/inputs/Password/Password";
 
 export default function Home() {
   return (
     <Layout title={HOME.display}>
       <main className={css.home}>
-        <h1>¡Hola mundo!</h1>
-        <Btn>Enviar</Btn>
-        <Email />
-        <Password />
+        <div className={css.hi}>
+          <h1>¡Hola mundo 👋!</h1>
+          <p>
+            Aprende de forma personalizada creando tus propias tarjetas de
+            estudio. Organiza un mazo, y comienza a aprender a tu ritmo.
+            Comparte tus mazos con la comunidad y ayuda a tus colegas a crecer
+            junto a ti.
+          </p>
+        </div>
       </main>
     </Layout>
   );
